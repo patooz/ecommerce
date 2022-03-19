0d2683e5f972e6d0b44cba6aa0a21edd2bbd3955
Patrick Kipsum <pat.koech01@gmail.com>
2022-03-19 13:27:12 +0300

    front end product details with multilanguage done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -994,10 +994,10 @@ $subcategories=App\Models\Subcategory::where('category_id',$category->id)->order
 
         @if ($product->discount_price == NULL)
 
-        <div class="product-price"> <span class="price"> Ksh{{$product->selling_price}} </span> </div>
+        <div class="product-price"> <span class="price"> Ksh {{$product->selling_price}} </span> </div>
 
             @else
-            <div class="product-price"> <span class="price">  Ksh{{$product->discount_price}} </span> <span class="price-before-discount"> Ksh{{$product->selling_price}}</span> </div>
+            <div class="product-price"> <span class="price">  Ksh {{$product->discount_price}} </span> <span class="price-before-discount"> Ksh {{$product->selling_price}}</span> </div>
 
             @endif
 
@@ -1053,45 +1053,71 @@ $subcategories=App\Models\Subcategory::where('category_id',$category->id)->order
   @endphp
           @forelse ($catwiseproducts as $product)
           <div class="item item-carousel">
-          <div class="products">
-              <div class="product">
-              <div class="product-image">
-                  <div class="image"> <a href="detail.html"><img  src="{{asset($product->product_thumbnail)}}" alt=""></a> </div>
-                  <!-- /.image -->
+            <div class="products">
+                <div class="product">
+                <div class="product-image">
+                    <div class="image"> <a href="detail.html"><img  src="{{asset($product->product_thumbnail)}}" alt=""></a> </div>
 
-                  <div class="tag new"><span>new</span></div>
-              </div>
-              <!-- /.product-image -->
+            @php
+                $amount=$product->selling_price - $product->discount_price;
+                $discount=($amount/$product->selling_price)*100;
+            @endphp
 
-              <div class="product-info text-left">
-                  <h3 class="name"><a href="detail.html"> {{ $product->product_name_en }} </a></h3>
-                  <div class="rating rateit-small"></div>
-                  <div class="description"></div>
-                  <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$ 800</span> </div>
-                  <!-- /.product-price -->
+            <div>
+                @if ($product->discount_price == NULL)
+                <div class="tag new"><span>new</span></div>
 
-              </div>
-              <!-- /.product-info -->
-              <div class="cart clearfix animate-effect">
-                  <div class="action">
-                  <ul class="list-unstyled">
-                      <li class="add-cart-button btn-group">
-                      <button data-toggle="tooltip" class="btn btn-primary icon" type="button" title="Add Cart"> <i class="fa fa-shopping-cart"></i> </button>
-                      <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-                      </li>
-                      <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
-                      <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
-                  </ul>
-                  </div>
-                  <!-- /.action -->
-              </div>
-              <!-- /.cart -->
-              </div>
-              <!-- /.product -->
+                @else
+                <div class="tag hot"><span>{{ round($discount) }}%</span></div>
 
-          </div>
-          <!-- /.products -->
-          </div>
+                @endif
+            </div>
+
+
+                </div>
+                <!-- /.product-image -->
+
+        <div class="product-info text-left">
+            <h3 class="name"><a href="detail.html">
+                @if (session()->get('language') == 'Kiswahili'){{ $product->product_name_swa}} @else {{ $product->product_name_en }} @endif
+                  </a></h3>
+            <div class="rating rateit-small"></div>
+            <div class="description"></div>
+
+            @if ($product->discount_price == NULL)
+
+            <div class="product-price"> <span class="price"> Ksh {{$product->selling_price}} </span> </div>
+
+                @else
+                <div class="product-price"> <span class="price">  Ksh {{$product->discount_price}} </span> <span class="price-before-discount"> Ksh {{$product->selling_price}}</span> </div>
+
+                @endif
+
+
+            <!-- /.product-price -->
+
+        </div>
+                <!-- /.product-info -->
+                <div class="cart clearfix animate-effect">
+                    <div class="action">
+                    <ul class="list-unstyled">
+                        <li class="add-cart-button btn-group">
+                        <button data-toggle="tooltip" class="btn btn-primary icon" type="button" title="Add Cart"> <i class="fa fa-shopping-cart"></i> </button>
+                        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+                        </li>
+                        <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+                        <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
+                    </ul>
+                    </div>
+                    <!-- /.action -->
+                </div>
+                <!-- /.cart -->
+                </div>
+                <!-- /.product -->
+
+            </div>
+            <!-- /.products -->
+            </div>
           <!-- /.item -->
 
           @empty
